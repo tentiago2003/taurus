736150aa3c88db0ac7b753046363f2e1f71ea386
tentiago2003 <tentiago2003@gmail.com>
2026-08-11 19:05:08 +0000

    feat: add WebSocket server

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const { WebSocketServer } = require('ws');
 
 const port = Number(process.env.PORT) || 3000;
 
@@ -6,6 +7,16 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Taurus API is running');
+});
+
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Taurus WebSocket client connected');
+  ws.send(JSON.stringify({
+    type: 'connection',
+    message: 'Taurus WebSocket connected',
+  }));
 });
 
 server.listen(port, () => {
